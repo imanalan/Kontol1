39116b0b7418b3f0a74b0a2e122bc98ef68dd71d
imanalan <154472299+imanalan@users.noreply.github.com>
2023-12-22 06:39:02 +0700

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 
 global.apikey = "";
 
-global.namalu = "FauziDevID";
-global.namabot = "Misaki"; 
-global.namaowner = "FauziDevID";
+global.namalu = "ArdhiXs";
+global.namabot = "aXsBOT"; 
+global.namaowner = "ArdhiXs";
 global.packname = namabot; 
 global.author = namaowner; 
-global.myweb = "https://s.id/oziioffc"; 
+global.myweb = "https://s.id/kontol"; 
 global.footer_text = namalu;
 global.pp_bot = fs.readFileSync("./src/media/misaki.jpg"); 
-global.owner = ["6289528652225"];
+global.owner = ["6288295249563"];
 global.sessionName = "session"; 
-global.gc = "https://chat.whatsapp.com/JuKwugAsvAYF1XKcRgmvyL"; 
+global.gc = "https://chat.whatsapp.com/KHrockYobMkuskyisp"; 
 global.background = "https://telegra.ph/file/5361453b7995459b67357.jpg";
 
 /**
